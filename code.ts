9d31e83f126191a2ababd48649ad162053e86d37
Miguel Solorio <miguel.solorio07@gmail.com>
2023-12-19 14:24:05 -0800

    Update

--- code.ts
+++ code.ts
@@ -31,11 +31,26 @@ figma.ui.onmessage = msg => {
                 a: number;
               } | any = hexToRgb(nestedVariable);
 
-              if (counter > 0) {
-                console.log('### 🔁 Inner loop')
-                const modeId = collectionParent.addMode(collectionMode)
+              console.log(`🚧 Counter: ${counter}, CollectionParenModes: ${collectionParent.modes.length}`)
 
-                let collection = figma.variables.getVariableById(collectionParent.variableIds[counter - 1])
+              if (counter > 0) {
+                // console.log('🔁 Inner loop')
+
+                if(collectionParent.modes.length == counter){
+                  console.log('🌓 Adding mode')
+                  collectionParent.addMode(collectionMode)
+                }
+
+                // TODO figure out how to update the first entry when a new mode is added, collection variable is null for second entry
+                const modeId = collectionParent.modes[counter].modeId;
+                console.log(counter)
+                let id = collectionParent.variableIds[counter]
+                // let collection = figma.variables.getVariableById(collectionParent.variableIds[counter])
+                let collection = figma.variables.getVariableById(id)
+                console.log(figma.variables.getVariableById(collectionParent.variableIds[counter]))
+                // console.log(`collectionParent: ${collectionParent}, Counter: ${counter}, modeId: ${modeId}`)
+                console.log(`collection: ${collection}`)
+
                 if (collection) {
                   collection.setValueForMode(modeId, {
                     r: color.r,
@@ -44,11 +59,13 @@ figma.ui.onmessage = msg => {
                     a: color.a
 
                   })
-                  console.log(`Added ${collectionVariable}: ${color} in ${collectionMode} `)
+                  console.log(`✅ Updated ${collection?.name} in ${collectionParent.modes[counter].name} with ${nestedVariable}`)
+                } else {
+
                 }
 
               } else {
-                console.log('### 1️⃣ Creating 1st variable')
+                // console.log(' 1️⃣ Creating 1st variable')
                 const newModeId = collectionParent.modes[counter].modeId;
                 collectionParent.renameMode(newModeId, collectionMode);
                 const collection = figma.variables.createVariable(
@@ -64,9 +81,10 @@ figma.ui.onmessage = msg => {
 
                 })
 
-                console.log(`Added ${collectionVariable}: ${color} in ${collectionMode} `)
+                console.log(`✅ Updated ${collection?.name} in ${collectionParent.modes[counter].name} with ${nestedVariable}`)
 
               }
+              console.log(figma.variables.getLocalVariables())
               counter++;
 
 
@@ -82,12 +100,10 @@ figma.ui.onmessage = msg => {
 
 function hexToRgb(hex: string): { r: number, g: number, b: number, a: number } | null {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(hex);
-    console.log(`Converting ${hex}`)
     return result ? {
         r: Math.round(parseInt(result[1], 16) / 2.55) / 100,
         g: Math.round(parseInt(result[2], 16) / 2.55) / 100,
         b: Math.round(parseInt(result[3], 16) / 2.55) / 100,
         a: result[4] ? Math.round(parseInt(result[4], 16) / 2.55) / 100 : 1
     } : null;
-    console.log(`Hex is ${result}`)
 }
